ab2a31effa276263b16ab791b39ac20f66ee2b27
helioVictor <heliohelio1@outlook.com>
2022-03-15 22:05:52 -0300

    Add maintenance page

--- handle_data.sh
+++ handle_data.sh
@@ -24,21 +24,21 @@ GET_PARTIONS_INFO=$(df -h | grep -v 'loop' | awk 'NR>1 {printf "%s %s %s\n",$2, 
 GET_INTERFACES_INFO=$(cat /proc/net/dev | awk 'NR>2 {printf "%s %s %s %s %s\n",$1, $2, $3, $10, $11}')
 
 echo "$GET_TIME" >> $FILENAME
-$(echo $GET_HOSTNAME >> $FILENAME)
-$(echo $GET_TOTAL_MEMORY_USED >> $FILENAME)
-$(echo $GET_TOTAL_MEMORY_AVALIABLE >> $FILENAME)
-$(echo $GET_PID_AND_PROCESS_NAME >> $FILENAME)
-$(echo $GET_PARTIONS_INFO >> $FILENAME)
-$(echo $GET_INTERFACES_INFO >> $FILENAME)
+echo $GET_HOSTNAME >> $FILENAME
+echo $GET_TOTAL_MEMORY_USED >> $FILENAME
+echo $GET_TOTAL_MEMORY_AVALIABLE >> $FILENAME
+echo $GET_PID_AND_PROCESS_NAME >> $FILENAME
+echo $GET_PARTIONS_INFO >> $FILENAME
+echo $GET_INTERFACES_INFO >> $FILENAME
 
-$(echo '<div class="card m-3">' >> $FILENAME_CODE)
-$(echo '    <div class="card-body">' >> $FILENAME_CODE)
-$(echo "            <p><b>Data (ISO8601):</b> $GET_TIME</p>" >> $FILENAME_CODE)
-$(echo "            <p><b>Nome da máquina:</b> $GET_HOSTNAME</p>" >> $FILENAME_CODE)
-$(echo "            <p><b>Total de memória RAM usada:</b> $GET_TOTAL_MEMORY_USED</p>" >> $FILENAME_CODE)
-$(echo "            <p><b>Total de memória RAM disponível:</b> $GET_TOTAL_MEMORY_AVALIABLE</p>" >> $FILENAME_CODE)
-$(echo "            <p><b>O nome do processo que utiliza mais memória e o total de memória RAM utilizada:</b> $GET_PID_AND_PROCESS_NAME</p>" >> $FILENAME_CODE)
-$(echo "            <p><b>A listagem das partições contendo ponto de ponto de montagem, tamanho e percentagem de utilização:</b> $GET_PARTIONS_INFO</p>" >> $FILENAME_CODE)
-$(echo "            <p><b>O total de bytes e pacotes enviados e recebidos em cada interface de rede:</b> $GET_INTERFACES_INFO</p>" >> $FILENAME_CODE)
-$(echo '        </div>' >> $FILENAME_CODE)
-$(echo '    </div>' >> $FILENAME_CODE)
+echo '<div class="card m-3">' >> $FILENAME_CODE
+echo '    <div class="card-body">' >> $FILENAME_CODE
+echo "            <p><b>Data [ISO8601]:</b> $GET_TIME</p>" >> $FILENAME_CODE
+echo "            <p><b>Nome da máquina:</b> $GET_HOSTNAME</p>" >> $FILENAME_CODE
+echo "            <p><b>Total de memória RAM usada:</b> $GET_TOTAL_MEMORY_USED</p>" >> $FILENAME_CODE
+echo "            <p><b>Total de memória RAM disponível:</b> $GET_TOTAL_MEMORY_AVALIABLE</p>" >> $FILENAME_CODE
+echo "            <p><b>O nome do processo que utiliza mais memória e o total de memória RAM utilizada:</b> $GET_PID_AND_PROCESS_NAME</p>" >> $FILENAME_CODE
+echo "            <p><b>A listagem das partições contendo ponto de ponto de montagem, tamanho e percentagem de utilização:</b> $GET_PARTIONS_INFO</p>" >> $FILENAME_CODE
+echo "            <p><b>O total de bytes e pacotes enviados e recebidos em cada interface de rede:</b> $GET_INTERFACES_INFO</p>" >> $FILENAME_CODE
+echo '        </div>' >> $FILENAME_CODE
+echo '    </div>' >> $FILENAME_CODE
